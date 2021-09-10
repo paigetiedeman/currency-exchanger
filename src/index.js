70c1bfbf0e27661fd4a70287a1a62b9874ab828e
Paige Tiedeman <paigetiedeman@gmail.com>
2021-09-10 12:38:33 -0700

    WIP: still having issues with UI and promise logic

--- src/index.js
+++ src/index.js
@@ -4,14 +4,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import currencyExchange from './currency-exchange.js';
 
-$(document).ready(function() {
-  
-  
-  
-  $("#currency-exchanger").submit(function (event) {
-    event.preventDefault();
-    let inputCurrency = $("#usCurrency").val();
-    $("#input").text('${inputCurrency}');
-    console.log(inputCurrency);
+function convertUS(inputCurrency, rate) {
+  return (inputCurrency * rate).toFixed(2);
+}
+
+$(document).ready(function () {
+  let promise = currencyExchange.getExchange(dollar);
+  promise.then(function (response) {
+    $('#currency-exchanger').submit(function (event) {
+      event.preventDefault();
+      // const body = JSON.parse(response);
+      const conversion = response.conversion_rates;
+      let inputCurrency = $('#usCurrency').val();
+      let selectedRate = $('#inputConversion').val();
+      let rate = conversion[selectedRate];
+      console.log(selectedRate);
+      $('#input').text(inputCurrency);
+      $('#converted').text(convertUS(inputCurrency, rate));
+    });
+  })(function (error) {
+    $('#error').text('${error}');
   });
 });
