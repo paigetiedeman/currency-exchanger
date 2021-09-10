7584648a9803017f842ce7e8ce56419a25165eb5
Paige Tiedeman <paigetiedeman@gmail.com>
2021-09-10 15:29:21 -0700

    getElements function added

--- src/currency-exchange.js
+++ src/currency-exchange.js
@@ -1,18 +1,16 @@
 export default class currencyExchange {
-  static getExchange() {
-    return new Promise(function (resolve, reject) {
-      let request = new XMLHttpRequest();
-      const url = 'https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD';
-      request.onload = function () {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(request.response);
+  static async getExchange(currency) {
+    try {
+      const response = await fetch('https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD');
+        if (!response.ok) {
+          throw Error(response.statusText);
         }
-      };
-      request.open("GET", url, true);
-      request.send();
-    });
+          return response.json();
+        }
+        catch(error) {
+          return error.message;
+        };
   }
 }
+
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import currencyExchange from './currency-exchange.js';
 
+function getElements(response, inputCurrency) {
+  if (response.result === "success") {
+    let exchange = (response.conversion_rates) * inputCurrency;
+    $('#input').text(inputCurrency);
+    $('#converted').text(exchange);
+  } else {
+    $('#error').text("There was an error: ${response}")
+  }
+}
 
 $(document).ready(function () {
   let promise = currencyExchange.getExchange();
@@ -15,13 +24,8 @@ $(document).ready(function () {
       let inputCurrency = $('#usCurrency').val();
       let selectedRate = $('#inputConversion').val();
       let rate = conversion[selectedRate];
-      let exchanger = new currencyExchange(inputCurrency, rate);
       const converter = exchanger.inputCurrency * exchanger.rate;
-      $('#input').text(inputCurrency);
-      $('#converted').text(converter);
+      
     });
   });
-  // (function(error) {
-  //   $('#error').text('${error}');
 });
-// });
