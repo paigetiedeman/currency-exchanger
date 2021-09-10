85397288f031a45bf35994a273950e50dd74f54c
Paige Tiedeman <paigetiedeman@gmail.com>
2021-09-10 10:29:39 -0700

    WIP: html and UI logic

--- src/currency-exchange.js
+++ src/currency-exchange.js
@@ -9,7 +9,7 @@ export default class currencyExchange {
         } else {
           reject(request.response);
         }
-      }
+      };
       request.open("GET", url, true);
       request.send();
     });
--- src/index.js
+++ src/index.js
@@ -4,3 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import currencyExchange from './currency-exchange.js';
 
+$(document).ready(function() {
+  
+  
+  
+  $("#currency-exchanger").submit(function (event) {
+    event.preventDefault();
+    let inputCurrency = $("#usCurrency").val();
+    $("#input").text('${inputCurrency}');
+    console.log(inputCurrency);
+  });
+});
