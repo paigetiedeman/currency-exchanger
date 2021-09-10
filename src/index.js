f5319e09692505b61edb721e4cff739b05bcf813
Paige Tiedeman <paigetiedeman@gmail.com>
2021-09-10 09:09:59 -0700

    basic HTML setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-// import $ from 'jquery';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './css/styles.css';
-// import  from './js/scripts.js';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import currencyExchange from './currency-exchange.js';
 
